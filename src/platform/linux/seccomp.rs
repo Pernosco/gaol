e50855b148a3eeb6ac56f8d7e1587c1b07622169
Robert O'Callahan <robert@ocallahan.org>
2021-09-17 01:51:42 +1200

    Allow faccessat2

--- src/platform/linux/seccomp.rs
+++ src/platform/linux/seccomp.rs
@@ -192,8 +192,9 @@ pub static ALLOWED_SYSCALLS: [c_long; 59] = [
     libc::SYS_writev,
 ];
 
-static ALLOWED_SYSCALLS_FOR_FILE_READ: [c_long; 16] = [
+static ALLOWED_SYSCALLS_FOR_FILE_READ: [c_long; 17] = [
     libc::SYS_access,
+    libc::SYS_faccessat2,
     libc::SYS_chdir,
     libc::SYS_fstat,
     libc::SYS_newfstatat, // SYS_fstatat64
